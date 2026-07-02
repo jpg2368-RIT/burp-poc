e00e4d5e83870995c810e2594cfccbf1b2499f0e
Joe <josephguarino2004@gmail.com>
2026-07-02 16:24:16 -0400

    added request intercept (finished vid pt 1)

--- src/main/java/Extension.java
+++ src/main/java/Extension.java
@@ -1,10 +1,24 @@
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.http.handler.*;
+import burp.api.montoya.http.message.requests.HttpRequest;
 
-public class Extension implements BurpExtension {
+public class Extension implements BurpExtension, HttpHandler {
     @Override
     public void initialize(MontoyaApi api) {
         api.extension().setName("Burp Suite POC Extension");
         api.logging().logToOutput("Extension successfully loaded :)");
+        api.http().registerHttpHandler(this);
+    }
+
+    @Override
+    public RequestToBeSentAction handleHttpRequestToBeSent(HttpRequestToBeSent requestToBeSent) {
+        HttpRequest request = requestToBeSent.withAddedHeader("New-Header", "MyHeaderValue");
+        return RequestToBeSentAction.continueWith(request);
+    }
+
+    @Override
+    public ResponseReceivedAction handleHttpResponseReceived(HttpResponseReceived responseReceived) {
+        return null;
     }
 }
