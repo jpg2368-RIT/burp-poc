b27938fb584c78426833576df18a144605829e5e
Joe <jpg2368@rit.edu>
2026-07-08 17:29:11 -0400

    Added menu options

--- src/main/java/MyExtension.java
+++ src/main/java/MyExtension.java
@@ -2,6 +2,8 @@ import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyExtension implements BurpExtension {
     @Override
@@ -22,8 +24,35 @@ public class MyExtension implements BurpExtension {
         api.extension().registerUnloadingHandler(new UnloadingHandler(handler));
 
         JPanel extPanel = new JPanel();
-        extPanel.add(new JLabel("Test"));
+        extPanel.setLayout(new BoxLayout(extPanel, BoxLayout.Y_AXIS));
 
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        menuItems.add(new MenuItem("API Endpoint", new JTextField("https://www.example.com")));
+        menuItems.add(new MenuItem("API Key", new JTextField("")));
+
+        for (MenuItem menuItem: menuItems) {
+            JPanel row = new JPanel();
+            row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
+
+            JLabel label = new JLabel(menuItem.label);
+            JComponent input = menuItem.input;
+
+            row.add(label);
+            row.add(input);
+
+            extPanel.add(row);
+        }
         api.userInterface().registerSuiteTab("Burp POC Extension Tab", extPanel);
+    }
+}
+
+class MenuItem {
+    public String label;
+    public JComponent input;
+
+    public MenuItem(String labelText, JComponent inputComponent) {
+        this.label = labelText;
+        this.input = inputComponent;
     }
 }
